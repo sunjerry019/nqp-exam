dd712e000c77f9aa96f7f7e92873bdb5cbbe67be
Simon Krumm <krsimon@web.de>
2023-07-17 21:03:59 +0200

    overhauled spectrum func with Plotter

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ import sys, os
 HOME_FOLDER = os.path.abspath(os.path.dirname(__file__))
 
 
-class lattice:
+class Lattice:
     def __init__(self, L: int) -> None:
         self.L = L
         self.Hamiltonian_manual = np.zeros((L + 1, L + 1))
@@ -27,6 +27,8 @@ class lattice:
                         self.Hamiltonian_manual[i][j] = -s
 
     def b_down_j(self, j: int):
+        assert j <= self.L - 1, "J too big"
+
         s1 = 0.5 * np.matrix([[0, 1], [1, 0]])
         s2 = 0.5 * np.matrix([[0, -1j], [1j, 0]])
         b = s1 + 1j * s2
@@ -38,64 +40,62 @@ class lattice:
         return b_final
 
     def correlator(self, j: int, l: int):
-        return self.b_down_j(j).getH() * self.b_down_j(l)
+        return self.b_down_j(j).getH() @ self.b_down_j(l)
 
     def build_hamiltonian_ed(self, t: float, s: float) -> None:
         for j in range(self.L - 1):
             # PBC
-            if j == self.L - 1:
+            if j == self.L - 2:
                 self.Hamiltonian += -t * (
-                    self.b_down_j(j).getH() * self.b_down_j(0)
-                    + self.b_down_j(j) * self.b_down_j(0).getH()
+                    self.b_down_j(j).getH() @ self.b_down_j(0)
+                    + self.b_down_j(j) @ self.b_down_j(0).getH()
                 )
             else:
                 self.Hamiltonian += -t * (
-                    self.b_down_j(j).getH() * self.b_down_j(j + 1)
-                    + self.b_down_j(j) * self.b_down_j(j + 1).getH()
+                    self.b_down_j(j).getH() @ self.b_down_j(j + 1)
+                    + self.b_down_j(j) @ self.b_down_j(j + 1).getH()
                 )
             # center hop part doesnt need any PBC separation
             self.Hamiltonian += -s * (
-                self.b_down_j(j).getH() * self.b_down_j(self.L - 1)
-                + self.b_down_j(j) * self.b_down_j(self.L - 1).getH()
+                self.b_down_j(j).getH() @ self.b_down_j(self.L - 1)
+                + self.b_down_j(j) @ self.b_down_j(self.L - 1).getH()
             )
 
     def spectrum(self, type: str, stepsize: float):
         # values of t and s for plot
-        t, s = np.mgrid[0.1:20:stepsize, 0.1:20:stepsize]
-        ts = np.vstack((t.flatten(), s.flatten())).T
-        # storage for evals
+        t = 1
+        s_t = np.power(10, np.linspace(start=-2, stop=1, num=100))
 
         # get eval for all ts combinations
-
         if type == "manual":
-            evals = np.ndarray((ts.shape[0], self.L + 1))
-            for i in range(ts.shape[0]):
-                self.build_hamiltonian_manual(ts[i][0], ts[i][1])
-                evals[i] = np.linalg.eigvals(test.Hamiltonian_manual)
+            evals = np.ndarray((len(s_t), self.L + 1))
+            for i in range(len(s_t)):
+                self.build_hamiltonian_manual(t, s=s_t[i] * t)
+                evals[i] = np.sort(np.linalg.eigvals(test.Hamiltonian_manual))
         else:
-            evals = np.ndarray((ts.shape[0], 2**self.L))
-            for i in range(ts.shape[0]):
-                self.build_hamiltonian_ed(ts[i][0], ts[i][1])
-                evals[i] = np.linalg.eigvals(test.Hamiltonian)
+            evals = np.ndarray((len(s_t), 2**self.L))
+            for i in range(len(s_t)):
+                self.build_hamiltonian_ed(t, s=s_t[i] * t)
+                evals[i] = np.sort(np.linalg.eigvals(test.Hamiltonian))
 
-        # compute s/t fraction for all ts combinations
-        ts_frac = [ts[i][1] / ts[i][0] for i in range(ts.shape[0])]
+        # reshape evals list according to the evolution of each eval under t_s
+        # evals_new = np.array([np.take(evals, n, 1) for n in range(self.L + 1)])
+        evals_new = evals.T
 
-        # reshape evals list according to the evolution of each eval under ts_frac
-        evals_new = np.array([np.take(evals, n, 1) for n in range(self.L + 1)])
-
-        P = Plotter(figsize=(6, 4), nrows=1, ncols=1, usetex=True)
+        num_eigv = len(evals_new)
+        P = Plotter(figsize=(6, 3 * num_eigv), nrows=num_eigv, ncols=1, usetex=True)
 
         # plot for all evals
-        for n in range(len(evals_new)):
-            evals_sort = [y for x, y in sorted(zip(ts_frac, evals_new[n]))]
-            ts_sort = [x for x, y in sorted(zip(ts_frac, evals_new[n]))]
-            P.ax.plot(
-                ts_sort,
-                evals_sort,
-                label="EV %.0f" % n,
+        for n in range(num_eigv):
+            # evals_sort = [y for x, y in sorted(zip(t_s, evals_new[n]))]
+            # ts_sort = [x for x, y in sorted(zip(t_s, evals_new[n]))]
+            P.ax[n].plot(
+                s_t,
+                evals_new[n],
+                label=f"EV {n}",
             )
-        P.ax.legend()
+            P.ax[n].legend()
+            P.ax[n].set_xscale("log")
         P.savefig(
             os.path.join(HOME_FOLDER, "..", "plots", "spectrum_" + str(type) + ".pdf")
         )
@@ -103,31 +103,31 @@ class lattice:
     def condensate_frac(self):
         n_0N_frac = []
         # values of t and s for plot
-        t, s = np.mgrid[0.1:20:1, 0.1:20:1]
-        ts = np.vstack((t.flatten(), s.flatten())).T
+        t = 1
+        s_t = np.linspace(start=0, stop=10, num=100)
+
         # storage for evals
         rho = np.ndarray((self.L, self.L))
-        for i in range(ts.shape[0]):
-            self.build_hamiltonian_ed(ts[i][0], ts[i][1])
+        for i in range(len(s_t)):
+            self.build_hamiltonian_ed(t=t, s=s_t[i] * t)
             evalues, evectors = np.linalg.eig(test.Hamiltonian)
             ground = evectors[:, np.where(evalues == min(evalues))][:, :, 0]
             for j in range(self.L):
                 for l in range(self.L):
-                    p = ground.conj().T * self.correlator(j, l) * ground
+                    p = ground.conj().T @ self.correlator(j, l) @ ground
                     rho[j][l] = np.real(p[0, 0])
 
             evalues = np.linalg.eigvals(rho)
             n_0N_frac.append(max(evalues) / np.trace(rho))
 
         P = Plotter(figsize=(6, 4), nrows=1, ncols=1)
-
-        ts_frac = [ts[i][1] / ts[i][0] for i in range(ts.shape[0])]
-        P.ax.scatter(ts_frac, n_0N_frac, s=3)
+        P.ax.scatter(s_t, n_0N_frac, s=3)
         P.savefig(os.path.join(HOME_FOLDER, "..", "plots", "condensate_fraction.pdf"))
 
 
-test = lattice(4)
-#'manual' gives the a) spectrum, anything else gives the c) spectrum
-test.spectrum("manual", 0.5)
-test.spectrum("exact", 1)
-test.condensate_frac()
+if __name__ == "__main__":
+    test = Lattice(4)
+    #'manual' gives the a) spectrum, anything else gives the c) spectrum
+    test.spectrum("manual", 0.5)
+    test.spectrum("exact", 1)
+    test.condensate_frac()
